f9e93b2865a43c7121f4b62ae39a8c1c733749a3
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-01-12 21:36:53 -0300

    operações simples de multiplicação

--- AulasBasicas/src/Aulas.java
+++ AulasBasicas/src/Aulas.java
@@ -48,7 +48,33 @@ public class Aulas {
 		System.out.println("o valor de c é 7");
 		System.out.println("o valor de d é 5");
 		System.out.println("o resultado da subtração é = " + sub);
+System.out.println();
 
+System.out.println("Agora vamos fazer umas multiplicações simples");
+//declarando as variáveis para fazer multiplicações
+
+int num1 = 7;
+int num2 = 8;
+int resultMult = num1 * num2;
+
+System.out.println("o valor do primeiro número declarado é 7");  
+System.out.println("o valor do segundo número  declarado  é 8");
+System.out.println("O resultado da multiplicação  entre 7 vezes 8 é = " +  resultMult);
+		
+
+System.out.println();
+System.out.println("Agora vamos aprender a fazer operação de divisão simples");
+
+//declarando os número para divisão
+
+int num3 = 25;
+int num4 = 5;
+		
+int resultDiv = num3 / num4;
+
+System.out.println("O valor do primeiro número declarado é  25");
+System.out.println("o valor do segundo número declarado é 5");
+System.out.println("o resultado da divisão entre 25 dividido por 5 é =" + resultDiv);
 	}
 
 }
